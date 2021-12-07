bb99f091b70d091e30cc1bfd704e4e316151afac
Ita Sulistiani <itasulis16@gmail.com>
2021-12-07 13:04:11 +0700

    adding recommendation feature

--- charts.py
+++ charts.py
@@ -1,6 +1,7 @@
 from threading import Condition
 import streamlit as st
 from helper import abc_classification, abc_xyz_class, filter_dataset, load_css, load_data, summary_poster, xyz, xyz_classification, xyz_summary
+from recommendation import get_recommendation
 
 st.set_page_config(layout='wide')
 st.title('ABC-XYZ Classification Dashboard')
@@ -52,9 +53,13 @@ coltab1, coltab2 = st.columns(2)
 with coltab1:
     st.markdown("**ABC-XYZ Analysis Classification Result**")
 
+    # combining the classification results
     abc_xyz_data = abc_xyz_class(abc_class, xyz_demand)
     st.write(abc_xyz_data)
 
 with coltab2:
-    st.markdown("**ABC-XYZ Analysis Classification Recommendation**")
-    # st.write(xyz_demand)
+    st.markdown("**ABC Analysis Reevaluation Recommendation**")
+
+    # getting recommendation
+    data_1 = get_recommendation(dataset, a_percentage, b_percentage)
+    st.write(data_1)
